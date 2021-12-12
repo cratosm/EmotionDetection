eb13563279d372210da8fedf7cb9760f6a924555
Leblanc Charles <charles.leblanc@epitech.eu>
2021-12-12 11:48:48 +0100

    [ADD][Beginning Prediction Feature] model classes Label Encoder

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,40 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import warnings # Supress Warnings
 import numpy as np
+import pickle
+from sklearn.preprocessing import LabelEncoder
+from matplotlib import image as img
+import tensorflow as tf
 from PIL import Image
 
 st.set_page_config(page_title="[Deep-Learning]", page_icon="👤", layout='centered', initial_sidebar_state='auto', menu_items=None)
 image = Image.open('emotion.png')
+
+def loadLabelEncoder(path):
+    le = LabelEncoder()
+    le.classes_ = np.load(path)
+    return le
+
+model = tf.keras.models.load_model('model.h5')
+le = loadLabelEncoder("class.npy")
+
+resize_and_rescale = tf.keras.Sequential([
+  tf.keras.layers.Resizing(48, 48),
+  tf.keras.layers.Rescaling(1./127.5, offset=-1),
+])
+
 def load_image(image_file):
     img = Image.open(image_file)
     return img
+
+def guessImage(image, le, model):
+    result = resize_and_rescale(image)
+    pred = model.predict(np.array([result]))
+    i = np.argmax(pred, axis=-1)
+    res = le.classes_[i]
+    print(res)
+    st.write("The predicted emotion is: " + res)
 
 st.title('[Deep-Learning] Emotion Detection')
 
@@ -26,6 +51,8 @@ if uploaded_file is not None:
     file_details = {"filename":uploaded_file.name, "filetype":uploaded_file.type,
                     "filesize":uploaded_file.size}
     st.write(file_details)
-
+    img_user = load_image(uploaded_file)
+    st.image(img_user,width=250)
+    image = img.imread(uploaded_file.name)
+    guessImage(image=image, le=le, model=model)
     # To View Uploaded Image
-    st.image(load_image(uploaded_file),width=250)
